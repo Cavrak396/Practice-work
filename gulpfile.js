1634e95446f072fcd194ac11257a34e6446cfb76
Cavrak396 <marko.kofs396@gmail.com>
2023-01-02 19:49:18 +0100

    changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 let gulp = require("gulp");
 let scss = require("gulp-sass");
 let scssLint = require("gulp-sass-lint");
-
+let iconfont = require('gulp-iconfont');
+let consolidate = require('gulp-consolidate');
 //1. Scss to scss
 
 gulp.task("scss", () => {
@@ -29,3 +30,35 @@ gulp.task("default", ['scss'], () => {
       .pipe(scssLint.format())
       .pipe(scssLint.failOnError());
   });
+
+  //3. Icon font 
+   
+  gulp.task("iconfont", () => {
+    return gulp
+      .src("src/svg/*.svg")
+      .pipe(
+        iconfont({
+          fontName: "iconfont",
+          formats: ["ttf", "eot", "woff", "woff2"],
+          appendCodepoints: true,
+          appendUnicode: false,
+          normalize: true,
+          fontHeight: 1000,
+          centerHorizontally: true,
+        })
+      )
+      .on("glyphs", function (glyphs, options) {
+        gulp
+          .src("src/iconfont-template/iconfont.scss")
+          .pipe(
+            consolidate("underscore", {
+              glyphs: glyphs,
+              fontName: options.fontName,
+              fontDate: new Date().getTime(),
+            })
+          )
+          .pipe(gulp.dest("src/scss/base/icon-font"));
+      })
+      .pipe(gulp.dest("dist/fonts"));
+  });
+  
