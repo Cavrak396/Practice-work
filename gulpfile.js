8291e56b7d23fc68a82fbed115e2ce8f76d6d85a
Cavrak396 <marko.kofs396@gmail.com>
2023-01-03 16:01:24 +0100

    changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 let gulp = require("gulp");
 let scss = require("gulp-sass");
 let scssLint = require("gulp-sass-lint");
-let iconfont = require('gulp-iconfont');
-let consolidate = require('gulp-consolidate');
+let iconfont = require("gulp-iconfont");
+let consolidate = require("gulp-consolidate");
 //1. Scss to scss
 
 gulp.task("scss", () => {
@@ -13,71 +13,71 @@ gulp.task("scss", () => {
 });
 
 //2. Watch task
-gulp.task("default", ['scss'], () => {
+gulp.task("default", ["scss"], () => {
   gulp.watch("src/**/*.scss"), ["scss"];
 });
 
-//3. Scss lint 
+//3. Scss lint
 
-  gulp.task("scss-lint",  () => {
-    return gulp
-      .src("src/scss/**/*.scss")
-      .pipe(
-        scssLint({
-          configFile: ".sass-lint.yml",
-        })
-      )
-      .pipe(scssLint.format())
-      .pipe(scssLint.failOnError());
-  });
-
-  //3. Icon font 
-   
-  gulp.task("iconfont", () => {
-    return gulp
-      .src("src/svg/*.svg")
-      .pipe(
-        iconfont({
-          fontName: "iconfont",
-          formats: ["ttf", "eot", "woff", "woff2"],
-          appendCodepoints: true,
-          appendUnicode: false,
-          normalize: true,
-          fontHeight: 1000,
-          centerHorizontally: true,
-        })
-      )
-      .on("glyphs", function (glyphs, options) {
-        gulp
-          .src("src/iconfont-template/iconfont.scss")
-          .pipe(
-            consolidate("underscore", {
-              glyphs: glyphs,
-              fontName: options.fontName,
-              fontDate: new Date().getTime(),
-            })
-          )
-          .pipe(gulp.dest("src/scss/base/icon-font"));
+gulp.task("scss-lint", () => {
+  return gulp
+    .src("src/scss/**/*.scss")
+    .pipe(
+      scssLint({
+        configFile: ".sass-lint.yml",
       })
-      .pipe(gulp.dest("dist/fonts"));
-  });
-  
-  //4. Gulp project-build
+    )
+    .pipe(scssLint.format())
+    .pipe(scssLint.failOnError());
+});
 
-  gulp.task("project-build", ["copy-html", "copy-img", "copyfonts"]);
+//3. Icon font
 
+gulp.task("iconfont", () => {
+  return gulp
+    .src("src/svg/*.svg")
+    .pipe(
+      iconfont({
+        fontName: "iconfont",
+        formats: ["ttf", "eot", "woff", "woff2"],
+        appendCodepoints: true,
+        appendUnicode: false,
+        normalize: true,
+        fontHeight: 1000,
+        centerHorizontally: true,
+      })
+    )
+    .on("glyphs", function (glyphs, options) {
+      gulp
+        .src("src/iconfont-template/iconfont.scss")
+        .pipe(
+          consolidate("underscore", {
+            glyphs: glyphs,
+            fontName: options.fontName,
+            fontDate: new Date().getTime(),
+          })
+        )
+        .pipe(gulp.dest("src/scss/base/icon-font"));
+    })
+    .pipe(gulp.dest("dist/fonts"));
+});
 
-  gulp.task("copy-html", () => {
-    return gulp.src("*.html")
-    .pipe(gulp.dest("dist"))
-  })
+//4. Gulp project-build
 
-  gulp.task('copy-img', () => {
-    return gulp.src('src/images/*.{gif,jpg,png,svg,webp}')
-        .pipe(gulp.dest('dist/images'));
-  });
+gulp.task("project-build", ["copy-html", "copy-img", "copyfonts"]);
 
-  gulp.task('copyfonts', () => {
-    gulp.src('src/fonts/**/*.{ttf,woff,eof,svg}')
-    .pipe(gulp.dest('dist/text-fonts'));
- });
+gulp.task("copy-html", () => {
+  return gulp.src("*.html").pipe(gulp.dest("dist"));
+});
+
+gulp.task("copy-img", () => {
+  return gulp
+    .src("src/images/*.{gif,jpg,png,svg,webp}")
+    .pipe(gulp.dest("dist/images"));
+});
+
+gulp.task("copyfonts", () => {
+  gulp
+    .src("src/fonts/**/*.{ttf,woff,eof,svg}")
+    .pipe(gulp.dest("dist/text-fonts"));
+});
